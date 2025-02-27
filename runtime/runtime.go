6cf098dab11050f6cc0fcff98e6cab77a820d462
nghiabadao <minhnghiabadao1002@gmail.com>
2025-02-27 11:39:22 +0700

    feat: api job

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1224,7 +1224,7 @@ type NakamaModule interface {
 	JobCreate(ctx context.Context, name, creatorId, timezone, schedule, executor string, input map[string]interface{}) (string, error)
 	JobDelete(ctx context.Context, creatorId, jobId string) error
 	JobUpdate(ctx context.Context, jobId, name, userId, timezone, schedule, executor string, content map[string]interface{})
-	JobList(ctx context.Context, userId, cursor string, limit int)
+	JobList(ctx context.Context, userId, cursor string, limit int) (*api.JobList, error)
 }
 
 /*
